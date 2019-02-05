929ca9b162a875d3b76863ab84ff333edbc8c824
Qasim Soomro <qasim@soomro.com>
2019-02-05 11:38:00 -0800

    chore: minor stuff

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/** Factories */
+
+/** Middleware */
+
+/** Routes */
+
+/** Instances */
+
+/** Trust proxy */
+
+/** Set middleware */
+
+/** Bootstrap router */
+
+/** Handle uncaught exceptions */
+
+/** Start app */
+
+/** Expose app */
